cd8a9d5e2216feaa4fc112546f56aa1093a54aa6
sunggyu74 <fdrack@naver.com>
2022-04-12 15:33:19 +0900

    20220412

--- Zombie/Assets/02.Scripts/PlayerMovement.cs
+++ Zombie/Assets/02.Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;            // 앞뒤 움직임의 속도
-    public float ratateSpeed = 180f;        // 좌우 회전 속도
+    public float rotateSpeed = 180f;        // 좌우 회전 속도
 
     private PlayerInput playerInput;        // 플레이어 입력을 알려주는 컴포넌트
     [SerializeField] private Rigidbody playerRigidbody;      // 플레이어 캐릭터의 리지드바디  
@@ -20,21 +20,31 @@ public class PlayerMovement : MonoBehaviour
         playerAnimator = GetComponent<Animator>();      // 제대로 할당 되었는지 어떻게 아는가?
     }
 
-    // FixedUpdate는 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리
+    // FixedUpdate는 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
     private void FixedUpdate()
     {
-        
+        Rotate();       // 회전실행
+        Move();         // 움직임 실행
+
+        // 입력값에 따라 애니메이터의 Move 파라미터값 변경
+        playerAnimator.SetFloat("Move", playerInput.move);
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
     private void Move()
     {
-
+        // 상대적으로 이동할 거리 계산
+        Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
+        // 리지드바디를 이용해 게임 오브젝트 위치 변경
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
     }
 
     // 입력값에 따라 캐릭터를 좌우로 회전
     private void Rotate()
     {
-
+        // 상대적으로 회전할 수치 계산
+        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
+        // 리지드바디를 이용해 게임 오브젝트 회전 변경
+        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
     }
 }
