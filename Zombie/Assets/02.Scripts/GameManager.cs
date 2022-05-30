882bc0d2733f7802e9600fc383d27a438b4ca89c
sunggyu74 <fdrack@naver.com>
2022-05-30 10:45:15 +0900

    220530_1

--- Zombie/Assets/02.Scripts/GameManager.cs
+++ Zombie/Assets/02.Scripts/GameManager.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -12,7 +11,6 @@ public class GameManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
